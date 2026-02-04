e0afcc974519f097c6f0f61b41405623e23f044f
hurry.woodvil@gmail.com <KIMURA Shunya>
2026-02-04 21:31:59 +0900

    add plugins about lsp
    - nvim-lspconfig
    - mason.nvim
    - mason-lspconfig

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      ensure_installed = {},
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        },
+      },
+    }
+  },
+}
