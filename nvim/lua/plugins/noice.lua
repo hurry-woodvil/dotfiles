481bb7dd6fc10e6c2789e90ad096d52e4fdac0e5
hurry.woodvil@gmail.com <KIMURA Shunya>
2026-02-05 16:08:13 +0900

    add plugin about notify
    - noice.nvim

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_liens"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+      cmdline = {
+        enabled = true,
+        view = "cmdline_popup",
+      },
+      message = {
+        enabled = true,
+        view = "popup",
+      },
+    },
+  },
+}
