2a5fc145ae3f13c5266b72d1da85a6a4ffb8cb27
hurry.woodvil@gmail.com <KIMURA Shunya>
2026-02-05 22:54:48 +0900

    refactor lsp files and update oil setting

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,24 +1,27 @@
 return {
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = {
-			"mason-org/mason.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		opts = {
-			ensure_installed = { "lua_ls", "stylua" },
-		},
-	},
-	{
-		"mason-org/mason.nvim",
-		opts = {
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗"
-				},
-			},
-		}
-	},
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      ensure_installed = {
+        require("config.language.lua").server_name,
+        require("config.language.lua").formatter_name,
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
 }
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,18 +2,24 @@ return {
   {
     "stevearc/oil.nvim",
     dependencies = {
-      "nvim-mini/mini.icons"
+      "nvim-mini/mini.icons",
     },
     keys = {
-      { "-",         "<cmd>Oil<cr>",                                             desc = "Open parent directory" },
-      { "<leader>e", function() require("oil").open(vim.fn.expand("%:p:h")) end, desc = "Open Oil at current file" }
+      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+      {
+        "<leader>e",
+        function()
+          require("oil").open(vim.fn.expand("%:p:h"))
+        end,
+        desc = "Open Oil at current file",
+      },
     },
     opts = {
       columns = {
         { "icon" },
         { "size" },
         { "mtime" },
-      }
+      },
     },
   },
 }
