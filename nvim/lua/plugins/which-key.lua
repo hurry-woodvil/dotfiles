de305c73d186e1680c79b5ab5d487975b06e9813
hurry.woodvil@gmail.com <KIMURA Shunya>
2026-02-05 18:19:13 +0900

    add plugin about key bind map
    - which-key.nvim

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      preset = "helix",
+      spec = {
+        mode = { "n", "x" },
+        { "<leader><tab>", group = "tabs" },
+        { "<leader>c",     group = "code" },
+        { "<leader>d",     group = "debug" },
+        { "<leader>dp",    group = "profiler" },
+        { "<leader>e",     group = "explorer" },
+        { "<leader>f",     group = "file/find" },
+        { "<leader>g",     group = "git" },
+        { "<leader>gh",    group = "hunks" },
+        { "<leader>q",     group = "quit/session" },
+        { "<leader>s",     group = "search" },
+        { "<leader>u",     group = "ui" },
+        { "<leader>x",     group = "diagnostics/quicfix" },
+        { "<leader>[",     group = "prev" },
+        { "<leader>]",     group = "next" },
+        { "<leader>g",     group = "goto" },
+        { "<leader>gs",    group = "surround" },
+        { "<leader>z",     group = "fold" },
+        {
+          "<leader>b",
+          group = "buffer",
+          expand = function()
+            return require("which-key.extras").expand.buf()
+          end,
+        },
+        {
+          "<leader>w",
+          group = "windows",
+          proxy = "<C-w>",
+          expand = function()
+            return require("which-key.extras").expand.win()
+          end,
+        },
+        { "gx", desc = "Open with system app" },
+      },
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (Which-key)",
+      },
+      {
+        "<c-w><space>",
+        function()
+          require("which-key").show({ keys = "<c-w>", loop = true })
+        end,
+        desc = "Window Hydra Mode (Which-key)",
+      },
+    },
+  },
+}
