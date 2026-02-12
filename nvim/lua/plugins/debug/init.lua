a298138aa89e8a7b3df80a486375183be4c25a01
hurry.woodvil@gmail.com <KIMURA Shunya>
2026-02-12 16:08:30 +0900

    modified Breakpoint key

--- nvim/lua/plugins/debug/init.lua
+++ nvim/lua/plugins/debug/init.lua
@@ -43,7 +43,7 @@ return {
       vim.keymap.set("n", "<F10>", dap.step_over)
       vim.keymap.set("n", "<F11>", dap.step_into)
       vim.keymap.set("n", "<F12>", dap.step_out)
-      vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<leader>bb", dap.toggle_breakpoint)
       vim.keymap.set("n", "<leader>B", function()
         dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
       end)
