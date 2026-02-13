2584fefa68fa116356a3d465c19c06e842bccae6
hurry.woodvil@gmail.com <KIMURA Shunya>
2026-02-13 09:54:01 +0900

    set diagnostics settings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,6 +20,26 @@ return {
     config = function(_, opts)
       require("mason-lspconfig").setup(opts)
 
+      -- diagnostic config
+      vim.diagnostic.config({
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+        },
+        severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.HINT] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+          },
+        },
+      })
+
       -- rust LSP ON
       vim.lsp.enable(require("config.language.rust").server_name)
     end,
