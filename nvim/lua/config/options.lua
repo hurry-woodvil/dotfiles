1c190d572e2068a54c81b3ec3975669d7c960194
hurry.woodvil@gmail.com <KIMURA Shunya>
2026-02-17 17:18:28 +0900

    modified vim settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,2 +1,15 @@
-vim.opt.clipboard = "unnamedplus"
-vim.opt.shiftwidth = 2
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.clipboard = "unnamedplus"
+opt.shiftwidth = 2
+
+opt.wrap = false
