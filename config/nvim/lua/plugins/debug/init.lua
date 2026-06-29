947e4922a1611fbd07317d90eaecd43066f8ad78
hurry-woodvil <hurry.woodvil@gmail.com>
2026-06-29 10:33:27 +0900

    rustfmt を実行

--- config/nvim/lua/plugins/debug/init.lua
+++ config/nvim/lua/plugins/debug/init.lua
@@ -1,9 +1,9 @@
 return {
   {
-    "mfussenegger/nvim-dap",
-    opts = require("utils.debug").get_opts(),
+    'mfussenegger/nvim-dap',
+    opts = require('utils.debug').get_opts(),
     config = function(_, opts)
-      local dap = require("dap")
+      local dap = require('dap')
 
       for name, adapter in pairs(opts.adapters or {}) do
         dap.adapters[name] = adapter
@@ -15,54 +15,54 @@ return {
     end,
   },
   {
-    "rcarriga/nvim-dap-ui",
+    'rcarriga/nvim-dap-ui',
     dependencies = {
-      "mfussenegger/nvim-dap",
-      "nvim-neotest/nvim-nio",
+      'mfussenegger/nvim-dap',
+      'nvim-neotest/nvim-nio',
     },
     config = function()
-      local dapui = require("dapui")
+      local dapui = require('dapui')
 
       dapui.setup()
 
-      local dap = require("dap")
-      dap.listeners.after.event_initialized["dapui"] = function()
+      local dap = require('dap')
+      dap.listeners.after.event_initialized['dapui'] = function()
         dapui.open()
       end
-      dap.listeners.before.event_terminated["dapui"] = function()
+      dap.listeners.before.event_terminated['dapui'] = function()
         dapui.close()
       end
-      dap.listeners.before.event_exited["dapui"] = function()
+      dap.listeners.before.event_exited['dapui'] = function()
         dapui.close()
       end
 
-      vim.keymap.set("n", "<leader>bt", function()
+      vim.keymap.set('n', '<leader>bt', function()
         dapui.toggle()
-      end, { desc = "Toggle DAP UI" })
-      vim.keymap.set("n", "<F5>", dap.continue)
-      vim.keymap.set("n", "<F10>", dap.step_over)
-      vim.keymap.set("n", "<F11>", dap.step_into)
-      vim.keymap.set("n", "<F12>", dap.step_out)
-      vim.keymap.set("n", "<leader>bb", dap.toggle_breakpoint)
-      vim.keymap.set("n", "<leader>B", function()
-        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end, { desc = 'Toggle DAP UI' })
+      vim.keymap.set('n', '<F5>', dap.continue)
+      vim.keymap.set('n', '<F10>', dap.step_over)
+      vim.keymap.set('n', '<F11>', dap.step_into)
+      vim.keymap.set('n', '<F12>', dap.step_out)
+      vim.keymap.set('n', '<leader>bb', dap.toggle_breakpoint)
+      vim.keymap.set('n', '<leader>B', function()
+        dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
       end)
     end,
   },
+  -- {
+  --   "jay-babu/mason-nvim-dap.nvim",
+  --   dependencies = {
+  --     "mfussenegger/nvim-dap",
+  --     "williamboman/mason.nvim",
+  --   },
+  --   opts = {
+  --     ensure_installed = { "codelldb", "python" },
+  --     automatic_installation = true,
+  --   },
+  -- },
   {
-    "jay-babu/mason-nvim-dap.nvim",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "williamboman/mason.nvim",
-    },
-    opts = {
-      ensure_installed = { "codelldb", "python" },
-      automatic_installation = true,
-    },
-  },
-  {
-    "theHamsta/nvim-dap-virtual-text",
-    dependencies = { "mfussenegger/nvim-dap" },
+    'theHamsta/nvim-dap-virtual-text',
+    dependencies = { 'mfussenegger/nvim-dap' },
     opts = {},
   },
 }
