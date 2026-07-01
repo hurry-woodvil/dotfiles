f61c5105500525fc1bcba7ea47b1a72eb1ced337
hurry-woodvil <hurry.woodvil@gmail.com>
2026-07-01 12:02:15 +0900

    neovim の環境変数を更新

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,12 @@
+vim.env.PATH = table.concat({
+  vim.fn.expand('~/.local/bin'),
+  '/opt/homebrew/opt/rustup/bin',
+  vim.fn.expand('~/.volta/bin'),
+  '/opt/homebrew/bin',
+  '/opt/homebrew/sbin',
+  vim.env.PATH,
+}, ':')
+
 require('config.options')
 require('config.keymaps')
 require('config.autocmd')
